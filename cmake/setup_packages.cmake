7838ff6670470fa569b9161770bcb5ad70bf2583
Anna Grabas <cat4anna@gmail.com>
2026-03-15 10:29:32 +0100

    Fix find boost

--- cmake/setup_packages.cmake
+++ cmake/setup_packages.cmake
@@ -1,8 +1,4 @@
 
-if(POLICY CMP0167)
-  cmake_policy(SET CMP0167 NEW)
-endif()
-
-find_package(Boost REQUIRED COMPONENTS program_options)
+find_package(Boost CONFIG REQUIRED COMPONENTS program_options)
 find_package(GTest CONFIG REQUIRED)
 find_package(benchmark CONFIG REQUIRED)
