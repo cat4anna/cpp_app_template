3e8773ebc58a256377a8ae2045bd1a34499b451c
Anna Grabas <cat4anna@gmail.com>
2026-03-27 11:37:27 +0100

    add multiplatform jenkinsfile

--- cmake/setup_packages.cmake
+++ cmake/setup_packages.cmake
@@ -1,5 +1,11 @@
 
-find_package(Boost CONFIG REQUIRED COMPONENTS program_options)
+if(EMSCRIPTEN)
+    set(CMAKE_THREAD_LIBS_INIT "-lpthread")
+    set(CMAKE_HAVE_THREADS_LIBRARY 1)
+    set(CMAKE_USE_WIN32_THREADS_INIT 0)
+    set(CMAKE_USE_PTHREADS_INIT 1)
+    set(THREADS_PREFER_PTHREAD_FLAG ON)
+endif()
 
 if (APP_DO_BENCHMARK)
     find_package(benchmark CONFIG REQUIRED)
@@ -8,3 +14,5 @@ endif()
 if (APP_DO_UNIT_TEST)
     find_package(GTest CONFIG REQUIRED)
 endif()
+
+find_package(Boost CONFIG REQUIRED COMPONENTS program_options)
